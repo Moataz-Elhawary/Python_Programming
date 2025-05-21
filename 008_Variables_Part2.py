2d9e7dd433eb3ea5d35912a9db52786df7daf026
Moataz Elhawary <mm201797@gmail.com>
2025-05-21 09:32:17 +0300

    Part2 of Var

---
+++ 008_Variables_Part2.py
@@ -0,0 +1,29 @@
+"""
+File: Variables_Part2.py
+Author: Moataz Elhawary
+Date: 2024-09-04 
+"""
+# ---------------------------------------------------------------
+# ---------------------- Variables ------------------------------
+# ---------------------------------------------------------------
+# Source Code : Original Code You Write it in Computer
+# Translation : Converting Source Code Into Machine Language
+# Compilation : Translate Code Before Run Time
+# Run-Time    : Period App Take To Executing Commands
+# Interpreted : Code Translated On The Fly During Execution
+# ---------------------------------------------------------------
+
+# Python is Dynamically typed Language => Can be change data Type In Run-Time ...
+x = 10
+x = "Hello"
+print(x)
+
+# Reserved Word
+help("keywords")
+
+# Assign Values
+a, b, c = 1, 2, 3
+
+print(a)
+print(b)
+print(c)
