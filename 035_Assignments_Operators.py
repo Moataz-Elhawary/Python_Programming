59638eea70cf3f1b248550f600a26cbdf0239ff8
Moataz Elhawary <mm201797@gmail.com>
2025-06-03 09:13:16 +0300

    Upload Assignments Operator

---
+++ 035_Assignments_Operators.py
@@ -0,0 +1,30 @@
+"""
+File: Assignment_Operators.py
+Author: Moataz Elhawary
+Date: 2024-09-10
+"""
+
+
+# --------------------------
+# -- Assignment Operators --
+# --------------------------
+# =
+# +=
+# -=
+# *=
+# /=
+# **=
+# %=
+# //=
+# --------------------------
+
+x = 10  # Var One
+y = 20  # Var Two
+
+# Var One = Self [Operator] Var Two
+# Var One [Operator]= Var Two
+
+# x += y
+x -= y
+
+print(x)
